ee0ef40117dc49f6ea6a66709abe5565d99f37d3
SimonAslund33 <114583924+SimonAslund33@users.noreply.github.com>
2024-06-05 14:40:49 +0200

    Add files via upload

--- jobscraperscript.py
+++ jobscraperscript.py
@@ -1,12 +1,12 @@
 from urllib.request import Request, urlopen
-from fake_useragent import UserAgent
+#from fake_useragent import UserAgent
 import random
 from bs4 import BeautifulSoup
-from IPython.core.display import clear_output
+#from IPython.core.display import clear_output
 import requests
 # Here I provide some proxies for not getting caught while scraping
-ua = UserAgent() # From here we generate a random user agent
-proxies = [] # Will contain proxies [ip, port]
+#ua = UserAgent() # From here we generate a random userS agent
+#proxies = [] # Will contain proxies [ip, port]
 
 # Main function
 def main():
@@ -19,7 +19,13 @@ def main():
   #print(results.prettify())
   job_elements = results.find_all("div", class_="card-content")
   for job_element in job_elements:
-    print(job_element, end="\n"*2)
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    print()
 if __name__ == '__main__':
   main()
 
